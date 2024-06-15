73fbf13ea2ce5b077de8c7cec6ae6abf0cbd35c8
Sudenur Meydan <74561611+SMeydan@users.noreply.github.com>
2024-06-15 17:54:46 +0300

    Update ClassesDealingwithComplexNumbers.py

--- ClassesDealingwithComplexNumbers.py
+++ ClassesDealingwithComplexNumbers.py
@@ -1,18 +1,26 @@
-import math
-
 class Complex(object):
     def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
         
     def __add__(self, no):
+        return Complex(self.real + no.real, self.imaginary + no.imaginary)
         
     def __sub__(self, no):
+        return Complex(self.real - no.real, self.imaginary - no.imaginary)
         
     def __mul__(self, no):
-
+        return Complex(((self.real * no.real)-(self.imaginary * no.imaginary)), ((self.real * no.imaginary)+(self.imaginary * no.real)))
+        
     def __truediv__(self, no):
-
+        divisor = no.real**2 + no.imaginary**2
+        real_part = (self.real * no.real + self.imaginary * no.imaginary) / divisor
+        imaginary_part = (self.imaginary * no.real - self.real * no.imaginary) / divisor
+        return Complex(real_part, imaginary_part)
+    
     def mod(self):
-
+        return Complex(math.sqrt(((self.imaginary**2) + (self.real**2))), 0)
+        
     def __str__(self):
         if self.imaginary == 0:
             result = "%.2f+0.00i" % (self.real)
@@ -27,9 +35,3 @@ class Complex(object):
             result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
         return result
 
-if __name__ == '__main__':
-    c = map(float, input().split())
-    d = map(float, input().split())
-    x = Complex(*c)
-    y = Complex(*d)
-    print(*map(str, [x+y, x-y, x*y, x/y, x.mod(), y.mod()]), sep='\n')
