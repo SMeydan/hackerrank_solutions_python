71f4142de7e69e9dcaf9004464dc5a96b29d9a78
Sudenur Meydan <74561611+SMeydan@users.noreply.github.com>
2024-06-15 15:44:02 +0300

    Create Setunion.py

---
+++ Setunion.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+size_e = int(input())
+english = input()
+size_f = int(input())
+french = input()
+
+english_set =set(english.split())
+french_set =set(french.split())
+
+print(len(english_set.union(french_set)))
