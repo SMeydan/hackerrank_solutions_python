d6c07ed2282b2ac80ced81d6a121ab33f9ca7d20
Sudenur Meydan <74561611+SMeydan@users.noreply.github.com>
2024-06-15 21:29:37 +0300

    Create AthleteSort.py

---
+++ AthleteSort.py
@@ -0,0 +1,20 @@
+
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+    
+    
+    sorted_array = sorted(arr, key=lambda x: x[k])
+    
+    for i in range(n):
+        print(' '.join(str(x) for x in sorted_array[i]))
