76821fba97c2e991c2f40cc562da1c5ad852186a
Sudenur Meydan <74561611+SMeydan@users.noreply.github.com>
2024-06-15 15:44:42 +0300

    Create Setintersection.py

---
+++ Setintersection.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+size_e = int(input())
+english = input()
+size_f = int(input())
+french = input()
+
+english_set =set(english.split())
+french_set =set(french.split())
+
+print(len(english_set.intersection(french_set)))
